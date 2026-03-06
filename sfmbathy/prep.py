b3c6ecdc4aceed595de08cd4ed314188a96276d7
teguhsulistian <teguhsulistian17@gmail.com>
2026-03-06 10:41:39 +0700

    Modify pyTMD version to 2.2.4 for enabling eotides function

--- sfmbathy/prep.py
+++ sfmbathy/prep.py
@@ -1,7 +1,10 @@
 import laspy
 import numpy as np
+from eo_tides.utils import list_models
+from pyTMD.io.model import load_database
 
 def load_las(file_path):
     las = laspy.read(file_path)
     pc = np.vstack([las.x, las.y, las.z]).T.astype("float32")
     return pc,las
+
