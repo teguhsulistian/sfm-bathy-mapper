591e0493249ac7ac51f171599b8ec0b50d2ab32d
teguhsulistian <teguhsulistian17@gmail.com>
2026-03-07 07:45:24 +0700

    modify .toml, add tide Water level calculation in exact time

--- sfmbathy/prep.py
+++ sfmbathy/prep.py
@@ -1,9 +1,12 @@
 import laspy
+import rasterio
 import pandas as pd
 import numpy as np
 from pyproj import Transformer
 from eo_tides.model import model_tides
+from rasterio.transform import rowcol
 
+## (1) Load LAS file and extract point cloud data, median coordinates, and CRS information ##
 
 def load_las(file_path):
     #input las file, return point cloud as numpy array and las object
@@ -22,7 +25,10 @@ def load_las(file_path):
   
     return pc,las, lon_med, lat_med
 
+
+## (2) Tide calculation to define water level##
 def tide_calc(tide_dir, model, x, y, start_time, end_time, freq):
+    #Calculate tides using the specified model and parameters, return tide results as a DataFrame
     tide_results = model_tides(
         x=x,
         y=y,
@@ -30,4 +36,24 @@ def tide_calc(tide_dir, model, x, y, start_time, end_time, freq):
         model=model,
         directory=tide_dir
     )
-    return tide_results
+
+    # Calculate mean tide height from the results
+    tide_mean = np.mean(tide_results['tide_height'])
+
+    
+    # Identify Ellipsoid height to MSL datum from the provided DATUM GeoTIFF file
+    tif_path = tide_dir + "/DATUM/IS_MSL_ELLIPSOID.tif"
+    with rasterio.open(tif_path) as src:
+        # Convert lat/lon to row/col pixel coordinates
+        row, col = rowcol(src.transform, x, y)
+        
+        # Read the value at that pixel
+        msl_ellips = src.read(1)[row, col]
+
+    WL = msl_ellips + tide_mean
+
+    print(f"Location Lon: {x:.6f}, Lat: {y:.6f} | time range: {start_time} to {end_time}")
+    print(f"Mean Tide Height: {tide_mean:.3f} m , refer to MSL")    
+    print(f"Representative Water Level: {WL:.3f} m , refer to Ellipsoid Reference")
+
+    return WL
